aa8c410484aaff17f399a537e7257538264d7d38
Dale <neophyt1983@gmail.com>
2019-09-26 18:45:20 -0500

    Placeholders In Chr_lib
    Added placeholders in chr_lib for a few random characters.

---
+++ chr_lib.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#Each character type is built as a function that takes configuration from the character
+
+#Declarations
+
+
+chr_imp()
+{
+	#This character is the basic imp which can be found on almost any level
+	echo "RAWR, I'm an imp"
+}
+
+chr_ent()
+{
+	#This is the ent which can mainly be found in wooded settings
+	echo "RAWR, I'm an ent"
+}
+
+chr_sucubus()
+{
+	#This is the sucubus which can mainly be found deep in the underworld and large cities
+	echo "RAWR, I'm a succubus"
+}
+
+chr_demon()
+{
+	#This is the demon which can be found anywhere but is mostly in the underworld
+	echo "RAWR, I'm a demon"
+}
+
+chr_orc()
+{
+	#This is the orc which can be found everywhere but dominantly in deserts
+	echo "RAWR, I'm an orc"
+}
+
+chr_fae()
+{
+	#This is a fae of which there are many varieties which exist in relevant places
+	echo "RAWR, I'm a fae"
+}
+
+chr_elf()
+{
+	#This is an elf which like the fae have a variety of types that exist in relevant places
+	echo "RAWR, I'm an elf"
+}
